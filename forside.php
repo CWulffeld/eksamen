ad4a5c3b46c35384ee045d557f8ea869d084cb8b
LauraLSJN <lsjn@ruc.dk>
2022-04-04 15:14:28 +0200

    update

--- forside.php
+++ forside.php
@@ -25,19 +25,42 @@ if (empty($_SESSION['user'])) { //tjekker om brugeren allerede er logget ind
          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
      <title> Forside </title>
+     <style >
+      body {
+       background-color:#DFDEDE;
+       font-family: Helvetica;
+        }
+
+      .btn-secondary{
+        background-color: #DFDEDE;
+        color: #212529;
+        }
+
+       h2{
+         color: #212529;
+         font-weight: bold;
+         font-size: 300%;
+       }
+
+       .card-body {
+         background-color: #FFF;
+         border-radius: 25px;
+       }
+
+     </style>
   </head>
       <body>
 
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
           <div class="container-fluid">
-            <a class="navbar-brand">Blog</a>
+            <a class="navbar-brand">WITS 2022</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="forside.php">Forside</a>
+                  <a class="nav-link" href="forside.php">Forside</a>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link" href="opretIndlæg.php">Opret indlæg</a>
@@ -65,40 +88,38 @@ if (empty($_SESSION['user'])) { //tjekker om brugeren allerede er logget ind
 ?>
 
 
-
-
-
-
-<div class='container mt-5'>
+<div class='container mt-5 mb-5'>
   <div class='row'>
-      <div class= 'col-md-6' style='background:red;' >
+      <div class= 'col-md-6'  >
+
+
 <?php
-echo "<h2> Dine informationer </h2>  <br>";
+echo "<h2> Din profil</h2>  <br>";
+echo "<div class='card'>"; //Laver tekstfeltet som card
+echo "<div class='card-body'>";
 echo "<b>Uid: </b>", $hentInfo['uid'], '<br>';
 echo "<b>Fornavn:  </b>", $hentInfo['firstname'], '<br>' ;
 echo "<b>Efternavn: </b>", $hentInfo['lastname'], '<br>';
-
-
-//echo "<h5 class='' style='color:white;'> uid: </h5>", $hentInfo['uid'];
-//echo "<h5 class='' style='color:white;'>". $hentInfo['uid']." </h5>" ;
+echo "</div>";
+echo "</div>";
 
 ?>
       </div>
 
+      <div class='col-md-6' >
 
-      <div class='col-md-6' style='background:blue;'>
 
           <?php
+          echo "<h2> Dine opslagstitler </h2>  <br>";
+
             foreach (get_pids_by_uid($user) as $pid){
               $getPost = get_post($pid);
-              /*echo "Titel ", $getPost['title'];
-              echo "<br>";*/
-
-              echo "<li> <b>Titlen er: </b> <a href='blogOpslag.php?pid=".$getPost['pid']."'>".$getPost['title']."</a> </li> <br>";
+                    echo "<div class='list-group'>
+                    <a
+                      class='list-group-item list-group-item-action' href='blogOpslag.php?pid=".$getPost['pid']."'>".$getPost['title']."</a> </div>";
 
             }
             ?>
-
       </div>
 
     </div>
