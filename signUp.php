c1b7880753580d4590d484ac8d3377623e03f964
LauraLSJN <lsjn@ruc.dk>
2022-04-13 12:30:11 +0200

    update

--- signUp.php
+++ signUp.php
@@ -13,6 +13,9 @@ Lavet af: Laura Sofie Juel Nielsen (LSJN) & Christine Wulffeld (CVANW)
 
     <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+        <!-- CSS stylesheet -->
+            <link rel="stylesheet" href="stylesheet.css" />
     <title> Opret bruger</title>
   </head>
   <body>
